708681ca873753270a995c0f723909c71951d04e
lyf911010 <lyf825848580@qq.com>
2017-01-07 18:25:03 +0800

    update

--- src/login/index.js
+++ src/login/index.js
@@ -1,9 +1,18 @@
 $('.doLogin').on('click',function(){
+    var username = $('#username').val();
+    var password = $('#inputPassword').val();
+    var param = {
+        userName:username,
+        passWord:password
+    };
     $.ajax({
         url:'http://127.0.0.1:3000/',
         type:'get',
+        data:param,
         success:function(data){
-            console.log(data);
+            if(data.data && (data.data == 'success')){
+                window.location.href = 'file:///F:/code/myblog/src/list/list.html'
+            }
         }
     })
 })
